264d9a358dd78b6ff72d6d9b9c866949e442064e
VPSMarket <admin@bestcheapvps.org>
2022-02-15 22:45:48 +0800

    add http api

--- info/rpc.go
+++ info/rpc.go
@@ -1,16 +1,18 @@
 package info
 
 import (
-	"context"
+	"bytes"
 	"fmt"
 	"github.com/cloudapp3/host/model"
 	"github.com/smallnest/rpcx/client"
-	"github.com/smallnest/rpcx/share"
+	"github.com/vmihailenco/msgpack/v5"
+	"io/ioutil"
+	"net/http"
 	"time"
 )
 
 const (
-	url = "172.245.81.122:9810"
+	url = "https://host.bestcheapvps.org/api/host/info"
 )
 
 var (
@@ -23,10 +25,10 @@ func SetToken(t string) {
 }
 
 func StartActions() {
-	connectRPCClient()
+	//connectRPCClient()
 	i := GetLinuxInfo()
 	reportHostInfo(&i)
-	t := time.NewTimer(time.Minute * 1)
+	t := time.NewTicker(time.Minute * 1)
 	for _ = range t.C {
 		i := GetLinuxInfo()
 		reportHostInfo(&i)
@@ -42,17 +44,41 @@ func connectRPCClient() {
 	}
 }
 func reportHostInfo(i *model.HostInfo) {
-	var reply string
-	ctx := context.WithValue(context.Background(), share.ReqMetaDataKey, map[string]string{"ip": clientIP,
-		"token": token})
-	err := c.Call(ctx, "host", "reportHostInfo", i, &reply)
+	httpClient := &http.Client{}
+	b, _ := msgpack.Marshal(i)
+	req, err := http.NewRequest("POST", url, bytes.NewReader(b))
 	if err != nil {
-		connectRPCClient()
-		fmt.Println("report host info client error " + err.Error())
+		fmt.Println("report http new request error " + err.Error())
+		return
 	}
-	if reply != "ok" {
-		fmt.Println("report host info reply not success " + reply)
-	}else{
-		fmt.Println("ok")
+	req.Header.Set("token", token)
+	req.Header.Set("ip", getClientIP())
+	rsp, rErr := httpClient.Do(req)
+	if rErr != nil {
+		fmt.Println("report request server error " + rErr.Error())
+		return
 	}
+	body, _ := ioutil.ReadAll(rsp.Body)
+	_ = rsp.Body.Close()
+	s := string(body)
+	if s != "ok" {
+		fmt.Println("report server info not success " + s)
+	}
+
 }
+
+//func reportHostInfo(i *model.HostInfo) {
+//	var reply string
+//	ctx := context.WithValue(context.Background(), share.ReqMetaDataKey, map[string]string{"ip": clientIP,
+//		"token": token})
+//	err := c.Call(ctx, "host", "reportHostInfo", i, &reply)
+//	if err != nil {
+//		connectRPCClient()
+//		fmt.Println("report host info client error " + err.Error())
+//	}
+//	if reply != "ok" {
+//		fmt.Println("report host info reply not success " + reply)
+//	}else{
+//		fmt.Println("ok")
+//	}
+//}
